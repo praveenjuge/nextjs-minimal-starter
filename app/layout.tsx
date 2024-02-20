98b9ad696e72adae9b15adb8e12ea3e93021230e
Praveen Juge <hello@praveenjuge.com>
2024-02-20 09:28:30 +0530

    Add Font
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
-import type { Metadata } from 'next';
+import { GeistSans } from 'geist/font/sans';
+import type { Metadata, Viewport } from 'next';
 import 'tailwindcss/tailwind.css';
+
+export const viewport: Viewport = { themeColor: '#000000' };
 
 export const metadata: Metadata = {
   title: 'TODO',
@@ -12,7 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${GeistSans.variable} bg-white text-slate-900 antialiased [font-family:var(--font-geist-sans)] [font-synthesis:none] [text-rendering:optimizeLegibility] [touch-action:manipulation]`}
+    >
       <body>{children}</body>
     </html>
   );
